2cd38e4419f368e8661850640c4e529f5f56ca19
LloydTao <LL23@live.co.uk>
2020-12-15 21:50:14 +0000

    Initialise engine tests

---
+++ engine/tests/keyboards/test_keyboards.py
@@ -0,0 +1,12 @@
+import unittest
+
+from ...keyboards import Keyboard
+
+
+class TestKeyboardMethods(unittest.TestCase):
+    def test_keyboard(self):
+        keyboard = Keyboard()
+
+
+if __name__ == "__main__":
+    unittest.main()
